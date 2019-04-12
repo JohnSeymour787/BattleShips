17540936f57e1dbd90b434dacff49d6ca72ffa74
Natalie <nataliesholakis@gmail.com>
2019-04-13 09:46:10 +1000

    Adding the correctly converted files

--- c#/src/DiscoveryController.cs
+++ c#/src/DiscoveryController.cs
@@ -1,82 +1,84 @@
+
+using Microsoft.VisualBasic;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+//using System.Data;
+using System.Diagnostics;
 using SwinGameSDK;
 
 /// <summary>
-
-/// ''' The battle phase is handled by the DiscoveryController.
-
-/// ''' </summary>
-namespace BattleShips
+/// The battle phase is handled by the DiscoveryController.
+/// </summary>
+static class DiscoveryController
 {
-	using static GameResources;
-	using static UtilityFunctions;
-	using static GameController;
 
-	static class DiscoveryController
+	/// <summary>
+	/// Handles input during the discovery phase of the game.
+	/// </summary>
+	/// <remarks>
+	/// Escape opens the game menu. Clicking the mouse will
+	/// attack a location.
+    /// Isuru: Updated Keycodes
+	/// </remarks>
+	public static void HandleDiscoveryInput()
 	{
-
-		/// <summary>
-		///     ''' Handles input during the discovery phase of the game.
-		///     ''' </summary>
-		///     ''' <remarks>
-		///     ''' Escape opens the game menu. Clicking the mouse will
-		///     ''' attack a location.
-		///     ''' </remarks>
-		public static void HandleDiscoveryInput()
-		{
-			if (SwinGame.KeyTyped(KeyCode.EscapeKey))
-				AddNewState(GameState.ViewingGameMenu);
-
-			if (SwinGame.MouseClicked(MouseButton.LeftButton))
-				DoAttack();
+        if (SwinGame.KeyTyped(KeyCode.EscapeKey)) {
+			GameController.AddNewState(GameState.ViewingGameMenu);
 		}
 
-		/// <summary>
-		///     ''' Attack the location that the mouse if over.
-		///     ''' </summary>
-		private static void DoAttack()
-		{
-			Point2D mouse;
-
-			mouse = SwinGame.MousePosition();
-
-			// Calculate the row/col clicked
-			int row, col;
-			row = Convert.ToInt32(Math.Floor((mouse.Y - FIELD_TOP) / (double)(CELL_HEIGHT + CELL_GAP)));
-			col = Convert.ToInt32(Math.Floor((mouse.X - FIELD_LEFT) / (double)(CELL_WIDTH + CELL_GAP)));
-
-			if (row >= 0 & row < HumanPlayer.EnemyGrid.Height)
-			{
-				if (col >= 0 & col < HumanPlayer.EnemyGrid.Width)
-					Attack(row, col);
-			}
-		}
-
-		/// <summary>
-		///     ''' Draws the game during the attack phase.
-		///     ''' </summary>s
-		public static void DrawDiscovery()
-		{
-			const int SCORES_LEFT = 172;
-			const int SHOTS_TOP = 157;
-			const int HITS_TOP = 206;
-			const int SPLASH_TOP = 256;
-
-			if (SwinGame.KeyDown(KeyCode.CKey) &&
-				( SwinGame.KeyDown(KeyCode.LeftShiftKey) ||
-				SwinGame.KeyDown(KeyCode.RightShiftKey) )
-			)
-				DrawField(HumanPlayer.EnemyGrid, ComputerPlayer, true);
-			else
-				DrawField(HumanPlayer.EnemyGrid, ComputerPlayer, false);
-
-			DrawSmallField(HumanPlayer.PlayerGrid, HumanPlayer);
-			DrawMessage();
-
-			SwinGame.DrawText(HumanPlayer.Shots.ToString(), Color.White, GameFont("Menu"), SCORES_LEFT, SHOTS_TOP);
-			SwinGame.DrawText(HumanPlayer.Hits.ToString(), Color.White, GameFont("Menu"), SCORES_LEFT, HITS_TOP);
-			SwinGame.DrawText(HumanPlayer.Missed.ToString(), Color.White, GameFont("Menu"), SCORES_LEFT, SPLASH_TOP);
+		if (SwinGame.MouseClicked(MouseButton.LeftButton)) {
+			DoAttack();
 		}
 	}
+
+	/// <summary>
+	/// Attack the location that the mouse if over.
+	/// </summary>
+	private static void DoAttack()
+	{
+		Point2D mouse = default(Point2D);
+
+		mouse = SwinGame.MousePosition();
+
+		//Calculate the row/col clicked
+		int row = 0;
+		int col = 0;
+		row = Convert.ToInt32(Math.Floor((mouse.Y - UtilityFunctions.FIELD_TOP) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
+		col = Convert.ToInt32(Math.Floor((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
+
+		if (row >= 0 & row < GameController.HumanPlayer.EnemyGrid.Height) {
+			if (col >= 0 & col < GameController.HumanPlayer.EnemyGrid.Width) {
+				GameController.Attack(row, col);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Draws the game during the attack phase.
+	/// </summary>
+    /// <remarks>
+    /// Isuru: Updated keycodes
+    /// </remarks>
+	public static void DrawDiscovery()
+	{
+		const int SCORES_LEFT = 172;
+		const int SHOTS_TOP = 157;
+		const int HITS_TOP = 206;
+		const int SPLASH_TOP = 256;
+
+        if ((SwinGame.KeyDown(KeyCode.LeftShiftKey) | SwinGame.KeyDown(KeyCode.RightShiftKey)) & SwinGame.KeyDown(KeyCode.CKey)) {
+			UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, true);
+		} else {
+			UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, false);
+		}
+
+		UtilityFunctions.DrawSmallField(GameController.HumanPlayer.PlayerGrid, GameController.HumanPlayer);
+		UtilityFunctions.DrawMessage();
+
+		SwinGame.DrawText(GameController.HumanPlayer.Shots.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SHOTS_TOP);
+		SwinGame.DrawText(GameController.HumanPlayer.Hits.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, HITS_TOP);
+		SwinGame.DrawText(GameController.HumanPlayer.Missed.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SPLASH_TOP);
+	}
+
 }
--- c#/src/GameLogic.cs
+++ c#/src/GameLogic.cs
@@ -1,35 +1,32 @@
+
+using Microsoft.VisualBasic;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+//using System.Data;
+using System.Diagnostics;
 using SwinGameSDK;
-
-namespace BattleShips
+static class GameLogic
 {
-	using static GameResources;
-	using static GameController;
-
-	class GameLogic
+	public static void Main()
 	{
-		public static void Main()
-		{
-			// Opens a new Graphics Window
-			SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
+		//Opens a new Graphics Window
+		SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
 
-			// Load Resources
-			GameResources.LoadResources();
+		//Load Resources
+		GameResources.LoadResources();
 
-			SwinGame.PlayMusic(GameMusic("Background"));
+		SwinGame.PlayMusic(GameResources.GameMusic("Background"));
 
-			// Game Loop
-			do
-			{
-				HandleUserInput();
-				DrawScreen();
-			}
-			while (!SwinGame.WindowCloseRequested() && CurrentState != GameState.Quitting);
+		//Game Loop
+		do {
+			GameController.HandleUserInput();
+			GameController.DrawScreen();
+		} while (!(SwinGame.WindowCloseRequested() == true | GameController.CurrentState == GameState.Quitting));
 
-			SwinGame.StopMusic();
+		SwinGame.StopMusic();
 
-			// Free Resources and Close Audio, to end the program.
-			FreeResources();
-		}
+		//Free Resources and Close Audio, to end the program.
+		GameResources.FreeResources();
 	}
 }
